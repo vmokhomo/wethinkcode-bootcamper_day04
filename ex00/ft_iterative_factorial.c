cf03809977d854b10e1da99dbf6a519cde30bed0
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-21 11:35:17 +0200

    day04/ex00

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmokhomo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 10:36:45 by vmokhomo          #+#    #+#             */
+/*   Updated: 2020/06/21 11:32:14 by vmokhomo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*	#include <unistd.h>
+**	#include <stdio.h>
+**
+**
+**  void ft_putchar(char c)
+**	{
+**		write(1, &c, 1);
+**	}
+*/
+
+int ft_iterative_factorial(int nb)
+{
+	int fact;
+	int i;
+
+	i = 1;
+	fact = 1;
+	if(nb < 0)
+	{
+		ft_putchar(1);
+	}
+	else
+	{
+		while(i <= nb)
+		{
+			fact = fact * i;
+			i++;
+		}
+		printf("%d", fact);
+	}
+	return(fact);
+}
+
+/*	int main()
+**	{
+**		return(ft_iterative_factorial(5));
+**	}
+*/
