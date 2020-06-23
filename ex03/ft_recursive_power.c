23232c7528f1a3295dac822476570f98e6d0422c
Victor Mokhomong <vmokhomo@c4r7s3.wethinkcode.co.za>
2020-06-23 12:20:54 +0200

    day04/ex03

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmokhomo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 15:02:06 by vmokhomo          #+#    #+#             */
+/*   Updated: 2020/06/22 15:07:12 by vmokhomo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+/*	void ft_putchar(char c)
+	{
+		write(1, &c, 1);
+	}
+*/
+int ft_recursive_power(int nb, int power)
+{
+	if(power != 0)
+		return(nb * ft_recursive_power(nb, power - 1));
+	else
+		return(1);	
+}
